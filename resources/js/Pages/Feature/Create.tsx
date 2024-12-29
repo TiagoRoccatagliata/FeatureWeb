5c2bdb0601c192d0d55c1e21cd5923f8d3c28e35
tiagoroccatagliata <tiagoroccatagliata@gail.com>
2024-12-28 23:48:08 -0300

    Fixing Build deplyment

--- resources/js/Pages/Feature/Create.tsx
+++ resources/js/Pages/Feature/Create.tsx
@@ -1,6 +1,7 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import {Head, useForm} from '@inertiajs/react';
-import {Feature} from "@/types";
+import {Feature, PaginatedData} from "@/types";
+import FeatureItem from "@/Components/FeatureItem";
 import InputLabel from "@/Components/InputLabel";
 import TextInput from "@/Components/TextInput";
 import InputError from "@/Components/InputError";
@@ -9,7 +10,13 @@ import TextAreaInput from "@/Components/TextAreaInput";
 import PrimaryButton from "@/Components/PrimaryButton";
 
 export default function Show() {
-  const {data, setData, processing, errors, post } = useForm({
+  const {
+    data,
+    setData,
+    processing,
+    errors,
+    post
+  } = useForm({
     name: '',
     description: ''
   })
@@ -21,51 +28,56 @@ export default function Show() {
       preserveScroll: true
     })
   }
-    return (
-      <AuthenticatedLayout
-        header={
-          <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-            Create New Feature
-          </h2>
-        }
-      >
-        <Head title="Create New Feature"/>
-        <div className="mb-4 overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
-          <div className="p-6 text-gray-900 dark:text-gray-100 flex gap-8">
-            <form onSubmit={createFeature} className="w-full">
-              <div className="mb-8">
-                <InputLabel htmlFor="name" value="Name"/>
 
-                <TextInput
-                  id="name"
-                  className="mt-1 block w-full"
-                  value={data.name}
-                  onChange={(e) => setData('name', e.target.value)}
-                  required
-                  isFocused
-                  autoComplete="name"
-                />
+  return (
+    <AuthenticatedLayout
+      header={
+        <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+          Create New Feature
+        </h2>
+      }
+    >
+      <Head title="Create New Feature"/>
 
-                <InputError className="mt-2" message={errors.name}/>
-              </div>
-              <div className="mb-8">
-                <InputLabel htmlFor="description" value="Description"/>
+      <div className="mb-4 overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
+        <div className="p-6 text-gray-900 dark:text-gray-100 flex gap-8">
+          <form onSubmit={createFeature} className="w-full">
+            <div className="mb-8">
+              <InputLabel htmlFor="name" value="Name"/>
 
-                <TextAreaInput
-                  id="description"
-                  className="mt-1 block w-full"
-                  value={data.description}
-                  onChange={(e) => setData('description', e.target.value)}
-                />
+              <TextInput
+                id="name"
+                className="mt-1 block w-full"
+                value={data.name}
+                onChange={(e) => setData('name', e.target.value)}
+                required
+                isFocused
+                autoComplete="name"
+              />
 
-                <InputError className="mt-2" message={errors.description}/>
-              </div>
-              <div className="flex items-center gap-4">
-                <PrimaryButton disabled={processing}>Save</PrimaryButton>
-              </div>
-            </form>
-          </div>
+              <InputError className="mt-2" message={errors.name}/>
+            </div>
+
+            <div className="mb-8">
+              <InputLabel htmlFor="description" value="Description"/>
+
+              <TextAreaInput
+                id="description"
+                rows={6}
+                className="mt-1 block w-full"
+                value={data.description}
+                onChange={(e) => setData('description', e.target.value)}
+              />
+
+              <InputError className="mt-2" message={errors.description}/>
+            </div>
+
+            <div className="flex items-center gap-4">
+              <PrimaryButton disabled={processing}>Save</PrimaryButton>
+            </div>
+          </form>
         </div>
-      </AuthenticatedLayout>
-);
+      </div>
+    </AuthenticatedLayout>
+  );
 }
